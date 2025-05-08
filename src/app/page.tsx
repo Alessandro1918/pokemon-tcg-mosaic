0a0b624225681f75ab8a6854ed3c06084e7ea5ca
Alessandro1918 <alessandro1918@hotmail.com>
2025-05-08 15:52:12 -0300

    feat: add to the grid the card with the best matching color of that section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,12 @@ export default function Home() {
 
   const [ cards, setCards ] = useState<CardProp[]>([])
 
-  const n = 25 //number of cards in the entire grid
+  const n = 11*11 //number of cards in the entire grid
 
   // const smImg = "https://www.npmjs.com/npm-avatar/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdmF0YXJVUkwiOiJodHRwczovL3MuZ3JhdmF0YXIuY29tL2F2YXRhci80MmJlZjI1ODU5ZWY1OTIyODUzYThmMmE3YzdhNGNlZj9zaXplPTEwMCZkZWZhdWx0PXJldHJvIn0.JTCwo1QFSJOROohvVLUAdrY_1A3z0vvpZJUB6gP-qh0"
-  // const bg = {deck: "SV3PT5", name: "Eevee", url: "https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV3PT5/SV3PT5_EN_133.png"}
+  // const eevee = "https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV3PT5/SV3PT5_EN_133.png"
+  // const tcgBack = "https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg"
+  
   const db = [
     {deck: "SV3PT5", name: "Jolteon",  avgColor: [217,202,124], url: "https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV3PT5/SV3PT5_EN_135.png"},
     {deck: "SM12",   name: "Flareon",  avgColor: [240,112,58],  url: "https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SM12/SM12_EN_25.png"},
@@ -30,27 +32,41 @@ export default function Home() {
     // setCards([db[1], db[2], db[3], db[4], db[5], db[1], db[2], db[3], db[4]]) // 9 items total, grid with 3 collumns, rows = 3 
 
     //Init grid - V2
-    for (let i = 0; i < n; i++) {
-      setTimeout(() => {
-        // console.log(cards.length % 5) //0, 0, 0, 0, 0, 0, 0, ... x infinite
-        // setCards([...cards, db[cards.length % 5]])
-        setCards(prevCards => {
-          // console.log(prevCards.length % 5) //0, 1, 2, 3, 4, 0, 1, 2, ...
-          return [...prevCards, db[prevCards.length % 5]];
-        })
-      }, i * 250)
-    }
+    // for (let i = 0; i < n; i++) {
+    //   setTimeout(() => {
+    //     // console.log(cards.length % 5) //0, 0, 0, 0, 0, 0, 0, ... x infinite
+    //     // setCards([...cards, db[cards.length % 5]])
+    //     setCards(prevCards => {
+    //       // console.log(prevCards.length % 5) //0, 1, 2, 3, 4, 0, 1, 2, ...
+    //       return [...prevCards, db[prevCards.length % 5]];
+    //     })
+    //   }, i * 250)
+    // }
 
     (async () => {
-      const data = await getImage("https://corsproxy.io/" + db[0].url)
+      const baseImageData = await getImage("https://corsproxy.io/" + "https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg")
 
-      //Split the image in small image sections, get the avgColor of each one:
+      //Split the base image in small image sections
       const sqrt = Math.sqrt(n) //400 images total? SqRt(400) = 20; and width * height = total
-      const sectionWidth = data.shape[0]/sqrt
-      const sectionHeight = data.shape[1]/sqrt
+      const sectionWidth = baseImageData.shape[0]/sqrt
+      const sectionHeight = baseImageData.shape[1]/sqrt
+
       for (let j = 0; j < sqrt; j++) {
         for (let i = 0; i < sqrt; i++) {
-          console.log(`x=${Math.trunc(i*sectionWidth)}, y=${Math.trunc(j*sectionHeight)}, avgColor=${getSectionAvgColor(data.pixels, data.shape[0], Math.trunc(i*sectionWidth), Math.trunc(j*sectionHeight), Math.trunc(sectionWidth), Math.trunc(sectionHeight))}`)
+          const avgColor = getSectionAvgColor(
+            baseImageData.pixels, baseImageData.shape[0], 
+            Math.trunc(i*sectionWidth), Math.trunc(j*sectionHeight), 
+            Math.trunc(sectionWidth), Math.trunc(sectionHeight))
+          const bestMatch = getBestColorFromDb(avgColor)
+          console.log(
+            `Section ${(i+1)+(j*sqrt)}:`,
+            `x=${Math.trunc(i*sectionWidth)}`,
+            `y=${Math.trunc(j*sectionHeight)}`,
+            `avgColor: ${avgColor}`,
+            `bestMatch: ${bestMatch.avgColor} (${bestMatch.name})`)
+            setCards(prevCards => {
+              return [...prevCards, bestMatch];
+            })
         }
       }
     })()
@@ -112,12 +128,38 @@ export default function Home() {
     return avg
   }
 
+  //For 2 given colors ([r, g, b], [...]), get their difference, from 0 (equal) to 1 (black to white).
+  function getColorDifference(color1: number[], color2: number[]) {
+    //V1 - Euclidian distance in the RGB 3-axys spaces:
+    return (
+      Math.abs(color1[0] - color2[0]) / 255 / 3 +
+      Math.abs(color1[1] - color2[1]) / 255 / 3 +
+      Math.abs(color1[2] - color2[2]) / 255 / 3
+    )
+  }
+
+  //For a given RBG color, returns the option with the most similar color from the list of cards.
+  function getBestColorFromDb(color: number[]) {
+    var bestMatch = db[0]
+    var diffFromBest = 1
+    db.map(card => {
+      const colorDiff = getColorDifference(color, card.avgColor)
+      // console.log(`Card: ${card.name}`, `Color difference: ${colorDiff}`)
+      if (colorDiff < diffFromBest) {
+        bestMatch = card
+        diffFromBest = colorDiff
+      }
+    })
+    // console.log(`Best match: ${bestMatch.name}`, `Color difference: ${diffFromBest}`)
+    return bestMatch
+  }
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>Hello, world!</h1>
       <div className={`
-        grid grid-cols-5
-        zgap-[4px] zbg-cover zbg-[url('https://assets.pokemon.com/static-assets/content-assets/cms2/img/cards/web/SV3PT5/SV3PT5_EN_133.png')]
+        grid grid-cols-11
+        zgap-[4px] bg-cover bg-[url('https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg')]
       `}>
         {
           cards.map((card, i) => {
@@ -125,7 +167,7 @@ export default function Home() {
               <img 
                 key={i}
                 src={card.url}
-                className="w-24 aspect-auto opacity-100"
+                className="w-10 aspect-auto opacity-70"
                 alt={`${card.deck} - ${card.name}`}
                 title={`${card.deck} - ${card.name}`}
               />
