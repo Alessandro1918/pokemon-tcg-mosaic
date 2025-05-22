8ab638b74022748b9b7921efc70582fe43805a60
Alessandro1918 <alessandro1918@hotmail.com>
2025-05-22 16:20:42 -0300

    style: add loading indicator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,13 +61,14 @@ export default function Home() {
   //(Because the grid section has the same ratio as the base image, grid-col count = grid-row count)
   // const gridSize = 50
   const [ gridSize, setGridSize ] = useState<25 | 50 | 75 | 100>(50)  //or: <keyof typeof gridConstants>
-
   const [ grid, setGrid ] = useState<GridCardProp[]>([])
+  const [ isGridLoading, setIsGridLoading ] = useState(false)
 
   const tcgBack = "https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg"
   const [ baseImage, setBaseImage ] = useState(tcgBack)
   const [ baseImageList, setBaseImageList ] = useState<ApiCardProp[]>([])
   const [ baseImageIndex, setBaseImageIndex ] = useState(-1)
+  const [ isBaseImageLoading, setIsBaseImageLoading ] = useState(false)
 
   //init @ zero, update only if client-side
   const [ zoomLevel, setZoomLevel ] = useState(0)   //will multiply section width by 4 to get size in px
@@ -94,9 +95,11 @@ export default function Home() {
   const cards = dataset
 
   function getBaseImageList(query: string) {
+    setIsBaseImageLoading(true)
     if (query == "TCG-back") {
       setBaseImageList([{id: "tcg-back", name: "-", set: { name: "-", series: "-" }, number: "-", image: tcgBack}])
       setBaseImageIndex(-1)
+      setIsBaseImageLoading(false)
       return
     }
     axios.get(`https://api.pokemontcg.io/v2/cards?q=name:${query}`).then(response => {
@@ -116,6 +119,7 @@ export default function Home() {
       })
       setBaseImageList(cards)
       setBaseImageIndex(0)
+      setIsBaseImageLoading(false)
     })
   }
 
@@ -126,6 +130,7 @@ export default function Home() {
   }
 
   async function makeMosaic() {
+    setIsGridLoading(true)
     setGrid([]) //reset grid
 
     const baseImageData = await getImage("https://corsproxy.io/" + baseImage)   //Dev
@@ -158,6 +163,7 @@ export default function Home() {
         })
       }
     }
+    setIsGridLoading(false)
   }
 
   //Updates baseImage preview everytime user changes the dropdown selection or picks a new option from the provided list
@@ -212,16 +218,27 @@ export default function Home() {
 
       {/* Slider: */}
       <div className="mt-2 flex flex-row items-center gap-4">
-        <button className="text-2xl font-bold disabled:text-gray-400 cursor-pointer disabled:cursor-default" disabled={!(baseImageIndex > 0 && baseImageIndex < baseImageList.length)} onClick={() => {if (baseImageIndex > 0 && baseImageIndex < baseImageList.length) {setBaseImageIndex(baseImageIndex - 1)}}}>{"<"}</button>
-        <img 
-          src={baseImage}
-          alt={getBaseImageName()}
-          title={getBaseImageName()}
-          className="w-40 aspect-auto"
-        />        
-        <button className="text-2xl font-bold disabled:text-gray-400 cursor-pointer disabled:cursor-default" disabled={!(baseImageIndex >= 0 && baseImageIndex < baseImageList.length -1)} onClick={() => {if (baseImageIndex >= 0 && baseImageIndex < baseImageList.length -1) {setBaseImageIndex(baseImageIndex + 1)}}}>{">"}</button>
+        <button className="text-4xl font-bold disabled:text-gray-400 cursor-pointer disabled:cursor-default" disabled={!(baseImageIndex > 0 && baseImageIndex < baseImageList.length)} onClick={() => {if (baseImageIndex > 0 && baseImageIndex < baseImageList.length) {setBaseImageIndex(baseImageIndex - 1)}}}>{"<"}</button>
+        {
+          isBaseImageLoading
+          ?
+            <div className="min-w-40 min-h-56 flex items-center justify-center">
+              <svg aria-hidden="true" className="w-12 h-12 mx-auto my-auto text-gray-300 dark:text-gray-300 fill-blue-600 animate-spin" viewBox="0 0 100 101" fill="none">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+            </div>
+          :
+            <img 
+              src={baseImage}
+              alt={getBaseImageName()}
+              title={getBaseImageName()}
+              className="w-40 aspect-auto"
+            /> 
+        }  
+        <button className="text-4xl font-bold disabled:text-gray-400 cursor-pointer disabled:cursor-default" disabled={!(baseImageIndex >= 0 && baseImageIndex < baseImageList.length -1)} onClick={() => {if (baseImageIndex >= 0 && baseImageIndex < baseImageList.length -1) {setBaseImageIndex(baseImageIndex + 1)}}}>{">"}</button>
       </div>
-      <h1 className="mt-1 mb-2 text-xs">{getBaseImageName()}</h1>
+      <h1 className="mt-1 mb-2 text-xs italic">{isBaseImageLoading ? "-" : getBaseImageName()}</h1>
       
       <button 
         onClick={() => makeMosaic()}
@@ -307,28 +324,37 @@ export default function Home() {
         // V3: <style> tag with { template string } inside JSX:
         className="grid"
       >
-        <div 
-          // className={
-          //   `grid grid-cols-50 min-w-max
-          //   zbg-cover zbg-[url('https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg')]`
-          // }
-          style={{ display: "grid", gridTemplateColumns: `repeat(${gridSize}, minmax(0, 1fr))`, minWidth: "max-content" }}  //CSS equivalent for the tailwind class above
-        >
-          {
-            grid.map((e, i) => {
-              return (
-                <img 
-                  key={i}
-                  src={e.url}
-                  alt={e.name}
-                  title={e.name}
-                  // className="w-5 aspect-auto opacity-100"
-                  style={{ width: `${zoomLevel * 4}px` }} //CSS equivalent for the tailwind class above
-                />
-              )
-            })
-          }
-        </div>
+        {
+          isGridLoading
+          ?
+            <svg aria-hidden="true" className="w-12 h-12 mx-auto my-auto text-gray-300 dark:text-gray-300 fill-blue-600 animate-spin" viewBox="0 0 100 101" fill="none">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+            </svg>
+          :
+            <div 
+              // className={
+              //   `grid grid-cols-50 min-w-max
+              //   zbg-cover zbg-[url('https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg')]`
+              // }
+              style={{ display: "grid", gridTemplateColumns: `repeat(${gridSize}, minmax(0, 1fr))`, minWidth: "max-content" }}  //CSS equivalent for the tailwind class above
+            >
+              {
+                grid.map((e, i) => {
+                  return (
+                    <img 
+                      key={i}
+                      src={e.url}
+                      alt={e.name}
+                      title={e.name}
+                      // className="w-5 aspect-auto opacity-100"
+                      style={{ width: `${zoomLevel * 4}px` }} //CSS equivalent for the tailwind class above
+                    />
+                  )
+                })
+              }
+            </div>
+        }
       </div>
     </div>
   )
