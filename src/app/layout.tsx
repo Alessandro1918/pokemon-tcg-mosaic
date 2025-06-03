38aeb1c502cbb00d0697f3c2daaf07e5d7f6341f
Alessandro1918 <alessandro1918@hotmail.com>
2025-06-03 15:49:26 -0300

    docs: new icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+//Icons: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons#image-files-ico-jpg-png
+
 export const metadata: Metadata = {
   title: "Pokemon TCG Mosaic",
   description: "https://github.com/Alessandro1918/pokemon-tcg-mosaic",
