777d54c5a84d176cc254cee7033fb43b2098e4d9
Alessandro1918 <alessandro1918@hotmail.com>
2025-06-04 23:30:03 -0300

    refactor: move getImageData from canvas function to separate file

--- src/app/api/og/route.ts
+++ src/app/api/og/route.ts
@@ -1,35 +1,29 @@
 import { NextResponse } from "next/server"
 import { createCanvas, loadImage } from "canvas"
+import { getImageV2 } from "../../functions/getImage"
 import getAvgColor from "../../functions/getAvgColor"
 import getBestColor from "../../functions/getBestColor"
-import dataset from "../../cards/eevolutions.json"
+import dataset from "../../cards/sv3pt5.json"
 
 export async function GET() {
 
-  const img = await loadImage("https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg")
+  const url = "https://i.ebayimg.com/images/g/evMAAOSwlRZflJ-g/s-l400.jpg"
 
-  //Init grid & canvas
-  const gridSize = 20
-  const gridWidth = 1.5 * img.width //px
-  const gridHeight = 1.5 * img.height //px
+  const baseImg = await getImageV2(url, 1.5)
+
+  const gridSize = 15               //Number of cards in the entire grid = size ˆ 2
+  const gridWidth = baseImg.width   //px
+  const gridHeight = baseImg.height //px
   const sectionWidth = Math.floor(gridWidth / gridSize)
   const sectionHeight = Math.floor(gridHeight / gridSize)
   const canvas = createCanvas(gridWidth, gridHeight)
   const ctx = canvas.getContext("2d")
 
-  //Get pixel data from target image
-  //V1: npm package "get-pixels"
-  // const imgData = await getImage("someURL")
-  //V2: from canvas
-  ctx.drawImage(img, 0, 0, gridWidth, gridHeight)
-  const imgData = ctx.getImageData(0, 0, gridWidth, gridHeight).data
-  ctx.clearRect(0, 0, gridWidth, gridHeight)
-
   for (let j=0; j<gridSize; j++) {
     for (let i=0; i<gridSize; i++) {
       // const img = await loadImage(dataset[i+gridSize*j].url)
       const avgColor = getAvgColor(
-        imgData, 
+        baseImg.data, 
         gridWidth, 
         Math.trunc(i*sectionWidth), 
         Math.trunc(j*sectionHeight), 
@@ -43,8 +37,8 @@ export async function GET() {
       //   `avgColor: ${avgColor} - `,
       //   `bestMatch: ${bestMatch.name} [${bestMatch.avgColor}]`
       // )
-      const img = await loadImage(bestMatch.url)
-      ctx.drawImage(img, i*sectionWidth, j*sectionHeight, sectionWidth, sectionHeight)
+      const bestImg = await loadImage(bestMatch.url)
+      ctx.drawImage(bestImg, i*sectionWidth, j*sectionHeight, sectionWidth, sectionHeight)
     }
   }
 
