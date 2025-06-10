56c034f776daf6dd8345fb769e6184f0c6b0d9aa
Alessandro1918 <alessandro1918@hotmail.com>
2025-06-10 16:34:05 -0300

    refactor: generate dinamic og image based on searchParams

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,17 @@
-import type { Metadata } from "next";
-import "./globals.css";
+// import type { Metadata } from "next"
+import "./globals.css"
 
 //Icons: https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons#image-files-ico-jpg-png
 
-export const metadata: Metadata = {
-  title: "Pokemon TCG Mosaic",
-  description: "https://github.com/Alessandro1918/pokemon-tcg-mosaic",
-  // metadataBase: new URL('https://pokemon-tcg-mosaic.vercel.app'),
-  openGraph: {
-    title: "Pokemon TCG Mosaic",
-    description: "https://github.com/Alessandro1918/pokemon-tcg-mosaic",
-    images: [{
-      // url: "/assets/og-image.jpg",  //relative to "/public"
-      url: "/api/og"
-    }],
-  }
-};
+//Using "generateMetadata" (from a server component) instead of "metadata" (from a static file)  
+// export const metadata: Metadata = {
+//   //...
+// }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
